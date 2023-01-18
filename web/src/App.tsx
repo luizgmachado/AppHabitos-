2c1087d61ac9f8709f57c6c18cbd77e9d325e190
Luiz Gustavo Machado <luiz.gustavo.machado@hotmail.com>
2023-01-18 20:52:20 -0300

    header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,19 @@
+import { Header } from './components/Header';
 import './styles/global.css';
-import { Habit } from "./components/Habit"
+//import { Habit } from "./components/Habit"
+
+
 
 export function App() {
   return (
-    <div>
-      <Habit completed={5} />
-      <Habit completed={5} />
-      <Habit completed={5} />
-      <Habit completed={89} />
-      <Habit completed={26} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        
+       <Header/>
+
+      </div>
    </div>
   
   )
 }
 
-// components: Reaproveitar / isolar
-// Propriedade: Uma informação enviada pra modificar um componente visual ou comportamentalmente 
